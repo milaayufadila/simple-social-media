ccd06241e21a67e393f5f032f18604a2db5037b4
[Mila Ayu Fadila] <[mayufadila@gmail.com]>
2026-04-28 19:40:02 +0000

    fix: updated install script

--- install.sh
+++ install.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
 set -e
 
-# Buat folder yang diperlukan
 mkdir -p bootstrap/cache storage/framework/cache storage/framework/sessions storage/framework/views
-
-# Install dependensi (ini yang paling penting)
 composer install --no-interaction --optimize-autoloader --no-dev
 npm install --legacy-peer-deps
 npm run build
-
-# Setup Laravel
 cp .env.example .env || true
 php artisan key:generate --force
-
-# Beri izin akses folder
 chown -R www-data:www-data bootstrap storage
 chmod -R 775 bootstrap storage
