dd53bac83a0c276f9f8c618402d5eedc7354c39d
[Mila Ayu Fadila] <[mayufadila@gmail.com]>
2026-04-29 03:45:39 +0000

    fix: simplify install script to avoid timeout

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
-
 mkdir -p bootstrap/cache storage/framework/cache storage/framework/sessions storage/framework/views
 composer install --no-interaction --optimize-autoloader --no-dev
-npm install --legacy-peer-deps
-npm run build
+# Kita beri tanda pagar (#) dulu di depan npm supaya tidak berat
+# npm install --legacy-peer-deps
+# npm run build
 cp .env.example .env || true
 php artisan key:generate --force
 chown -R www-data:www-data bootstrap storage
