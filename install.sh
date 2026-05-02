4a78ea7740d6e48a56ceddc5a04fdadccc87c3cc
[Mila Ayu Fadila] <[mayufadila@gmail.com]>
2026-05-02 09:39:34 +0000

    Disable migration during build

--- install.sh
+++ install.sh
@@ -18,5 +18,5 @@ php artisan key:generate
 sed -i 's/DB_HOST=127.0.0.1/DB_HOST=172.17.0.2/g' .env
 sed -i 's/DB_PASSWORD=/DB_PASSWORD=password/g' .env
 
-php artisan migrate --force
-php artisan db:seed --force
+#php artisan migrate --force
+#php artisan db:seed --force
